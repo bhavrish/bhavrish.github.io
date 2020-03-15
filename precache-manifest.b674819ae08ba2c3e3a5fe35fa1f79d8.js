534600bbce2a71ea740a238f82613e5e1eb34370
bhavrish <bhavrish@gmail.com>
2020-03-14 23:24:48 -0400

    Updates

---
+++ precache-manifest.b674819ae08ba2c3e3a5fe35fa1f79d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "813a8bc8612c69796b86d5227b2a675a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40fdaf4fb139c23196d9",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bceada028e06603e4ded",
+    "url": "/static/js/2.0f2a5e8f.chunk.js"
+  },
+  {
+    "revision": "40fdaf4fb139c23196d9",
+    "url": "/static/js/main.847984f8.chunk.js"
+  },
+  {
+    "revision": "cdcfdae3163b5e22da79",
+    "url": "/static/js/runtime~main.b05ea253.js"
+  }
+]);
