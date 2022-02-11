382b0fbd7342d0632ffadfc3b70ead95b2e33272
bhavrish <bhavrish@gmail.com>
2022-02-11 17:26:29 -0600

    Updates

---
+++ precache-manifest.c61fd5f48b024e9b9016e2c3d5b8477e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c63c6bcaccf29bf1c089aef82e746ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae6bebc8b2e14ce3f75",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7a8a47cb9df147bd8a74",
+    "url": "/static/js/2.bee29dd2.chunk.js"
+  },
+  {
+    "revision": "4ae6bebc8b2e14ce3f75",
+    "url": "/static/js/main.81ee2fa7.chunk.js"
+  },
+  {
+    "revision": "cdcfdae3163b5e22da79",
+    "url": "/static/js/runtime~main.b05ea253.js"
+  }
+]);
--- src/resumeData.js
+++
@@ -1,6 +0,0 @@
-let resumeData = {
-  "imagebaseurl":"http://bhavrish.github.io/",
-  "name": "Bhavesh Shah",
-  "role": "Software Engineer"
-  }
-export default resumeData
